0a50f53fec92a207913372d69208423245824c49
ninamars <48785892+ninamars@users.noreply.github.com>
2021-07-14 13:32:31 +0300

    Update ukb_effects_prscs.R

--- ukb_effects_prscs.R
+++ ukb_effects_prscs.R
@@ -9,8 +9,6 @@
 
 # South Asian and African / Caribbean
 
-/fs/projects/finngen/incoming_data/nmars/apps/R-3.4.2/bin/R
-
 library(data.table)
 library(dplyr)
 library(stringi)
@@ -18,28 +16,23 @@ library(pROC)
 library(ROCR)
 library(ggplot2)
 
-d <- fread("/fs/projects/ukbb/nmars/phenos/phenos_intervene_20201026.txt")
+d <- fread("phenos_intervene_20201026.txt")
 
-cad_prs <- fread("/fs/projects/ukbb/nmars/R6_prscs_cad.add.160614.website_UKB_nonwhite_PRS_20210615.txt")
+cad_prs <- fread("prscs_cad.add.160614.website_UKB_nonwhite_PRS_20210615.txt")
 names(cad_prs) <- c("eid", "cadprs")
 d <- merge(d, cad_prs, by = "eid")
 
-brcanc_prs <- fread("/fs/projects/ukbb/nmars/finngen_R5_michailidoubreastcancerall_UKB_nonwhite_PRS_20210615.txt")
+brcanc_prs <- fread("michailidoubreastcancerall_UKB_nonwhite_PRS_20210615.txt")
 names(brcanc_prs) <- c("eid", "brcancprs")
 d <- merge(d, brcanc_prs, by = "eid")
 
-prcanc_prs <- fread("/fs/projects/ukbb/nmars/R6_prscs_meta_v3_onco_euro_overall_ChrAll_1_release_UKB_nonwhite_PRS_20210615.txt")
+prcanc_prs <- fread("prscs_meta_v3_onco_euro_overall_ChrAll_1_release_UKB_nonwhite_PRS_20210615.txt")
 names(prcanc_prs) <- c("eid", "prcancprs")
 d <- merge(d, prcanc_prs, by = "eid")
 
-t2d_prs <- fread("/fs/projects/ukbb/nmars/R6_prscs_finngen_R6_METAANALYSIS_DIAGRAM_SE1_UKB_nonwhite_PRS_20210615.txt")
+t2d_prs <- fread("prscs_METAANALYSIS_DIAGRAM_SE1_UKB_nonwhite_PRS_20210615.txt")
 names(t2d_prs) <- c("eid", "t2dprs")
 d <- merge(d, t2d_prs, by = "eid")
-
-table(d$eth2)
-tapply(d$baselineage, d$eth2, summary)
-tapply(d$baselineage, d$eth2, sd, na.rm = T)
-tapply(d$sex, d$eth2, function(x){ prop.table(table(x)) })
 
 # -------------------------------------------------------------------------
 
@@ -60,6 +53,10 @@ formula <- formula(paste0("chd ~ scale(cadprs) + ", covs))
 fit <- glm(formula, data = temp, family = "binomial")
 summary(fit)
 
+#exp(coefficients(fit)[2])
+#exp(coefficients(fit)[2] - 1.96*summary(fit)$coefficients[2,2])
+#exp(coefficients(fit)[2] + 1.96*summary(fit)$coefficients[2,2])
+
 # -------------------------------------------------------------------------
 
 # Type 2 diabetes
@@ -78,10 +75,6 @@ sd(d$diabetes_age[d$eid %in% temp$eid], na.rm = T)
 formula <- formula(paste0("diabetes ~ scale(t2dprs) + ", covs))
 fit <- glm(formula, data = temp, family = "binomial")
 summary(fit)
-
-#exp(coefficients(fit)[2])
-#exp(coefficients(fit)[2] - 1.96*summary(fit)$coefficients[2,2])
-#exp(coefficients(fit)[2] + 1.96*summary(fit)$coefficients[2,2])
 
 # -------------------------------------------------------------------------
 
@@ -127,39 +120,24 @@ summary(fit)
 
 # EUR ancestry
 
-/fs/projects/finngen/incoming_data/nmars/apps/R-3.4.2/bin/R
 
-library(data.table)
-library(dplyr)
-library(stringi)
-library(pROC)
-library(ROCR)
-library(ggplot2)
+d <- fread("phenos_intervene_20201026.txt")
 
-d <- fread("/fs/projects/ukbb/nmars/phenos/phenos_intervene_20201026.txt")
-
-cad_prs <- fread("/fs/projects/ukbb/nmars/cad.add.160614.website_UKB_R6_PRS_20201105.txt")
+cad_prs <- fread("cad.add.160614.website_UKB_PRS_20201105.txt")
 names(cad_prs) <- c("eid", "cadprs")
 d <- merge(d, cad_prs, by = "eid")
 
-brcanc_prs <- fread("/fs/projects/ukbb/nmars/prscs_michailidoubreastcancerall_UKB_PRS_20201028.txt")
+brcanc_prs <- fread("prscs_michailidoubreastcancerall_UKB_PRS_20201028.txt")
 names(brcanc_prs) <- c("eid", "brcancprs")
 d <- merge(d, brcanc_prs, by = "eid")
 
-prcanc_prs <- fread("/fs/projects/ukbb/nmars/onco_euro_overall_ChrAll_1_release_UKB_R6_PRS_20201105.txt")
+prcanc_prs <- fread("onco_euro_overall_ChrAll_1_release_UKB_PRS_20201105.txt")
 names(prcanc_prs) <- c("eid", "prcancprs")
 d <- merge(d, prcanc_prs, by = "eid")
 
-t2d_prs <- fread("/fs/projects/ukbb/nmars/METAANALYSIS_DIAGRAM_SE1_UKB_R6_PRS_20201105.txt")
+t2d_prs <- fread("METAANALYSIS_DIAGRAM_SE1_UKB_PRS_20201105.txt")
 names(t2d_prs) <- c("eid", "t2dprs")
 d <- merge(d, t2d_prs, by = "eid")
-
-dim(d) # 343676
-
-table(d$eth2)
-tapply(d$baselineage, d$eth2, summary)
-tapply(d$baselineage, d$eth2, sd, na.rm = T)
-tapply(d$sex, d$eth2, function(x){ prop.table(table(x)) })
 
 # -------------------------------------------------------------------------
 
